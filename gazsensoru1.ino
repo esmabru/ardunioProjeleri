28cdd10aa5f5287b92d78499a23f755cbe8d84c1
esmabru <esmaebruozturk@gmail.com>
2025-05-24 19:29:21 +0300

    Add files via upload

---
+++ gazsensoru1.ino
@@ -0,0 +1,18 @@
+int buzzer=13;
+int gazSensoru=A0;
+int deger;
+
+void setup(){
+pinMode(buzzer,OUTPUT);
+pinMode(gazSensoru,INPUT); 
+}
+void loop(){
+deger=analogRead(gazSensoru);
+  if(deger>1000){
+    tone(buzzer,1000);
+     delay(1000);
+  } else{
+    noTone(buzzer);
+  }
+  delay(1000);
+}
