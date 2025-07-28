45b1546212a4e5939107d49123bc102cb5bdffa9
esmabru <esmaebruozturk@gmail.com>
2025-07-28 17:43:51 +0300

    Update pirProjeleri.cpp

--- pirProjeleri.cpp
+++ pirProjeleri.cpp
@@ -24,3 +24,35 @@ void loop()
   
   delay(1000); 
 }
+/* Sistem,yalnızca ortam karanlık olduğunda ve hareket algılandığında LED lambayı (veya ampulü) belirli bir süre için yakar.
+Böylece gereksiz elektrik tüketiminin önüne geçilir.*/
+int ldr=A0;
+int led=7;
+int pir=2;
+int deger=0;
+int ldr_deger;
+
+
+void setup()
+{
+  pinMode(led,OUTPUT);
+  pinMode(pir,INPUT);
+  pinMode(ldr,INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  deger=digitalRead(pir);
+  Serial.println(deger);
+  ldr_deger=analogRead(ldr);
+  Serial.println(ldr_deger);
+
+  //Eğer hareket algılanıyorsa ve ortam karanlıksa led yanar
+  if((deger==HIGH) && (ldr_deger>500)){
+  digitalWrite(led,HIGH);
+  delay(5000);
+  }
+  else
+  digitalWrite(led,LOW);
+}
