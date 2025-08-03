0c8d61e1f9559f77a5db6e5218b9242a723e21d1
esmabru <esmaebruozturk@gmail.com>
2025-08-03 14:57:53 +0300

    Update LDRprojeleri.cpp

--- LDRprojeleri.cpp
+++ LDRprojeleri.cpp
@@ -20,3 +20,21 @@ void loop()
     digitalWrite(led,LOW);
   }
 }
+//Ldr Sensör ile Karanlığa Doğru Led'in Artarak Yanması
+int led=5;
+int ldr=A0;
+
+void setup()
+{
+  pinMode(led, OUTPUT);
+  pinMode(ldr, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  Serial.println(analogRead(ldr));
+  //2 ye bölme sebebimiz analog değerin 0-1023 arası bir değer okuyacak olması biz bu değeri daha da minimuma indiriyoruz çünkü analog write 0-255 arasını kabul eder
+  analogWrite(led,analogRead(ldr)/2);
+  delay(10);
+}
