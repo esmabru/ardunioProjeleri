df9f05b80c666009420eb3c11952a90fadfef079
esmabru <esmaebruozturk@gmail.com>
2025-08-03 16:35:36 +0300

    Update LDRprojeleri.cpp

--- LDRprojeleri.cpp
+++ LDRprojeleri.cpp
@@ -38,3 +38,95 @@ void loop()
   analogWrite(led,analogRead(ldr)/2);
   delay(10);
 }
+
+//LDR Sensör İle 5 Led'i Sırayla Yakma
+int led1=5;
+int led2=6;
+int led3=7;
+int led4=8;
+int led5=9;
+int ldr=A0;
+int ldrDeger;
+
+void setup()
+{
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(ldr, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  ldrDeger=analogRead(ldr);
+  Serial.println(ldrDeger);
+  
+  if(ldrDeger<1020 && ldrDeger>882){
+  digitalWrite(led1,HIGH);
+  digitalWrite(led2,LOW);
+  digitalWrite(led3,LOW);
+  digitalWrite(led4,LOW);
+  digitalWrite(led5,LOW);
+    delay(300);
+  } 
+  if(ldrDeger<882 && ldrDeger>748){
+  digitalWrite(led1,HIGH);
+  digitalWrite(led2,HIGH);
+  digitalWrite(led3,LOW);
+  digitalWrite(led4,LOW);
+  digitalWrite(led5,LOW);
+    delay(300);
+  } 
+  if(ldrDeger<748 && ldrDeger>613){
+  digitalWrite(led1,HIGH);
+  digitalWrite(led2,HIGH);
+  digitalWrite(led3,HIGH);
+  digitalWrite(led4,LOW);
+  digitalWrite(led5,LOW);
+    delay(300);
+  } 
+  if(ldrDeger<613 && ldrDeger>479){
+  digitalWrite(led1,HIGH);
+  digitalWrite(led2,HIGH);
+  digitalWrite(led3,HIGH);
+  digitalWrite(led4,HIGH);
+  digitalWrite(led5,LOW);
+    delay(300);
+  } 
+  if(ldrDeger<479 && ldrDeger>344){
+  digitalWrite(led1,HIGH);
+  digitalWrite(led2,HIGH);
+  digitalWrite(led3,HIGH);
+  digitalWrite(led4,HIGH);
+  digitalWrite(led5,HIGH);
+    delay(300);
+  }  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
