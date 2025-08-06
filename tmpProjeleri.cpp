cef4f1e8f0be788bfa1151eb1f92748f1d3492be
esmabru <esmaebruozturk@gmail.com>
2025-08-06 15:39:09 +0300

    Update tmpProjeleri.cpp

--- tmpProjeleri.cpp
+++ tmpProjeleri.cpp
@@ -24,3 +24,46 @@ void loop()
     delay(200);
   }
 }
+
+//Sıcaklık Sensörü İle Led Yakma
+int kirmizi=5;
+int mavi=6;
+int yesil=7;
+int tmp=A0;
+int sicaklik;
+
+void setup()
+{
+  pinMode(kirmizi, OUTPUT);
+  pinMode(mavi, OUTPUT);
+  pinMode(yesil, OUTPUT);
+  pinMode(tmp, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+ sicaklik=analogRead(tmp);
+ Serial.println(sicaklik);
+  
+  if(sicaklik<30){
+  digitalWrite(kirmizi,LOW);
+  digitalWrite(mavi,LOW);
+  digitalWrite(yesil,LOW);
+  }
+  else if(sicaklik>30 && sicaklik<100){
+  digitalWrite(kirmizi,HIGH);
+  digitalWrite(mavi,LOW);
+  digitalWrite(yesil,LOW);
+  }
+  else if(sicaklik>100 && sicaklik<200){
+  digitalWrite(kirmizi,HIGH);
+  digitalWrite(mavi,HIGH);
+  digitalWrite(yesil,LOW);
+  }
+  else if(sicaklik>200){
+  digitalWrite(kirmizi,HIGH);
+  digitalWrite(mavi,HIGH);
+  digitalWrite(yesil,HIGH);
+  }
+}
