ec70b2ea381440d23eabfb745c1c40061ea62ea3
esmabru <esmaebruozturk@gmail.com>
2025-09-01 17:11:46 +0300

    Update gazsensoruProjeleri.cpp

--- gazsensoruProjeleri.cpp
+++ gazsensoruProjeleri.cpp
@@ -17,3 +17,44 @@ deger=analogRead(gazSensoru);
   }
   delay(1000);
 }
+
+//Lcd Ekranlı Gaz Alarmı
+#include <LiquidCrystal.h>
+int dcMotor=7;
+int gazSensoru=A0;
+int gazDegeri;
+int buzzer=13;
+int led=10;
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup()
+{
+ lcd.begin(16, 2); 
+ pinMode(gazSensoru,INPUT);
+ pinMode(buzzer,OUTPUT);
+ pinMode(led,OUTPUT);
+ lcd.begin(16,2); 
+ Serial.begin(9600);
+}
+
+void loop()
+{
+ gazDegeri=analogRead(gazSensoru);
+ Serial.println(gazDegeri);
+  
+  if(gazDegeri>150){
+    digitalWrite(dcMotor,HIGH);
+    digitalWrite(led,HIGH);
+    tone(buzzer,1000);
+  }
+  else{
+    digitalWrite(dcMotor,LOW);
+    digitalWrite(led,LOW);
+    noTone(buzzer);
+  }
+  lcd.setCursor(0,0);
+  lcd.print("Gaz degeri= ");
+  lcd.println(gazDegeri);
+  delay(100);
+}
