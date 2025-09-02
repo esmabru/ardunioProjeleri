429c8c0efd2832bded8ed75307cfcb25baa11f46
esmabru <esmaebruozturk@gmail.com>
2025-09-03 00:33:10 +0300

    Update kizilotesiKumandaProjeleri.cpp

--- kizilotesiKumandaProjeleri.cpp
+++ kizilotesiKumandaProjeleri.cpp
@@ -2,33 +2,36 @@
 #include <IRremote.h>  // IR kumanda ile haberleşme için gerekli kütüphane
 
 // Uzaktan kumandadaki tuşların HEX kodları
-#define BUTTON_OFF  0xF30CBF00  // LED kapatma tuşu
-#define BUTTON_ON   0xEF10BF00  // LED açma tuşu
+#define buton0  0xF30CBF00  // LED kapatma
+#define buton1  0xEF10BF00  // LED açma
 
-#define IR_PIN 2   // IR alıcının bağlı olduğu Arduino pini
-int ledPin = 5;  // LED'in bağlı olduğu Arduino pini
+#define kizilotesi 2 // IR alıcının bağlı olduğu Arduino pini
+int led = 5;  // LED'in bağlı olduğu Arduino pini
 
 void setup() {
-  pinMode(ledPin, OUTPUT); 
+  pinMode(led, OUTPUT); 
   Serial.begin(9600); 
-  IrReceiver.begin(IR_PIN, ENABLE_LED_FEEDBACK); // IR alıcıyı başlat ve LED geri bildirimini etkinleştir
+  IrReceiver.begin(kizilotesi, ENABLE_LED_FEEDBACK); // IR alıcıyı başlat ve LED geri bildirimini etkinleştir
 }
 
 void loop() {
   // Eğer IR kumandadan bir sinyal gelirse
   if (IrReceiver.decode()) {
-    unsigned long receivedCode = IrReceiver.decodedIRData.decodedRawData; // Gelen kodu al
+     unsigned long deger = IrReceiver.decodedIRData.decodedRawData; // Gelen kodu al
 
     // Gelen kodu kontrol et ve LED'i aç/kapat
-    if (receivedCode == BUTTON_ON) {
-      digitalWrite(ledPin, HIGH); 
+    if (deger == buton1) {
+      digitalWrite(led, HIGH);
       Serial.println("LED Açık");
     }
-    else if (receivedCode == BUTTON_OFF) {
-      digitalWrite(ledPin, LOW);
+    else if (deger == buton0) {
+      digitalWrite(led, LOW);
       Serial.println("LED Kapalı");
     }
 
     IrReceiver.resume(); // Bir sonraki IR sinyalini almak için alıcıyı hazırla
   }
 }
+
+  
+   
